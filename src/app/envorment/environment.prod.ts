ab42478f52f6e27c3d3bef7531fb5632b4b3c68c
SiddharthPotpose <siddharthpotpose9@gmail.com>
2026-02-24 20:00:27 +0530

    new changes

--- src/app/envorment/environment.prod.ts
+++ src/app/envorment/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment ={
     production : true,
-    apiUrl : 'https://freeapi.miniprojectideas.com/api/CarRentalApp/'
+        apiUrl : 'https://corsproxy.io/?https://freeapi.miniprojectideas.com/api/CarRentalApp'
+
 }
--- src/app/envorment/environment.ts
+++ src/app/envorment/environment.ts
@@ -1,4 +1,4 @@
 export const environment ={
     production : false,
-    apiUrl : 'https://freeapi.miniprojectideas.com/api/CarRentalApp'
+    apiUrl : 'https://corsproxy.io/?https://freeapi.miniprojectideas.com/api/CarRentalApp'
 }
